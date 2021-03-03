6a678978836f8632617149e27e035e4461ca2c2f
seahi <me@seahi.me>
2021-03-03 17:41:42 +0800

    修复两个静态文件的路径

--- footer.php
+++ footer.php
@@ -32,8 +32,8 @@
     </section>
 </div>
 
-<script src="https://seahi.me/usr/themes/untitled/static/baguetteBox.js"></script>
-<script src="https://seahi.me/usr/themes/untitled/static/script.js"></script>
+<script src="https://seahi.me/usr/themes/XuanMing/static/baguetteBox.js"></script>
+<script src="https://seahi.me/usr/themes/XuanMing/static/script.js"></script>
 <script> window.addEventListener('load', function() { baguetteBox.run('body',{ overlayBackgroundColor:'hsla(0,0%,100%,.92)', noScrollbars: true }) }); </script>
 
 </body>
