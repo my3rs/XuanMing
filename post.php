3e6631682290385f3442b98255dc24d0ec0fd07d
seahi <me@seahi.me>
2021-03-04 22:56:57 +0800

    fix 不显示评论

--- post.php
+++ post.php
@@ -7,7 +7,7 @@
             <div class="post posts post-page">
                 <div class="pure-g">
                     <div class="pure-u-1 pure-u-md-7-8 pure-u-lg-3-4 post-body">
-                        <h1 class="post-title"></h1>
+                        <h1 class="post-title"><?php $this->title(); ?></h1>
                         <div class="post-content">
                             <?php $this->content(); ?>
                         </div>
