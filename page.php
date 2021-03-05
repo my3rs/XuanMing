1342e37e0503d4bd083b7c748a62aba323ba690e
seahi <me@seahi.me>
2021-03-05 14:44:35 +0800

    fix 独立页面显示异常

--- page.php
+++ page.php
@@ -1,1 +1,27 @@
-<?php
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
+<?php $this->need('header.php'); ?>
+
+<section id="content">
+    <div class="body_container">
+        <div id="layout">
+            <div class="pure-g">
+                <div class="pure-u-1 pure-u-md-7-8 pure-u-lg-3-4 page-body">
+                    <div class="post post-page">
+                        <div class="post-head">
+                            <h1 class="post-title"><?php $this->title(); ?></h1>
+                            <span class="views-num"><?php $this->commentsNum('%d 条评论'); ?></span>
+                        </div>
+                        <div class="post-content">
+                            <?php $this->content(); ?>
+                        </div>
+
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php $this->need('footer.php'); ?>
